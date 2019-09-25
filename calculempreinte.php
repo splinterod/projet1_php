e7e991be65535ad467a2d8d988c6d9d936990dc7
julien <julienl6900669006@gmail.com>
2019-09-25 16:13:22 +0200

    footer et header en include php dans index.php , calcul.php et eradique.php

---
+++ calculempreinte.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="styleP1.css" rel="stylesheet">
+    <title>Commit tree</title>
+    
+</head>
+<body>
+    <!-- affichage du header -->
+    <?php require("header.html"); ?>
+    
+
+    <!-- debut baniere, section laurent-->
+<div class="container">
+  <div class="logoleft">
+  <img class="pvr" src="Images/planete_verre.jpg" class="logo_header" alt="logo de commit tree">
+  <img class="pvr" src="Images/orig_exp.jpg" class="logo_header" alt="logo de commit tree">
+</div>
+
+    <section id="calculco">
+    <h1>Calcule ton empreinte de CO2</h1>
+      <form action="" method="post">
+        <div id="formulaire">
+          <label for="">Nombre de Km Annuel</label>
+          <input class="premier" type="text" name="CO2 émis">
+        </div>
+        <div id="formulaire">
+          <label for="nom">Nombre de personnes dans le foyer</label>
+          <input class="second" type="text" name="nb arbres">
+        </div>
+
+        <div id="formulaire">
+        <label for="nom">Montant de mon achat :</label>
+        <input class="trois " type="text" name="nb arbres">
+        </div>
+
+        <div id="formulaire"> 
+          <label for="message">Mes actions personnelles :</label>
+          <textarea type="text" name="Montant"></textarea>
+        </div>
+       
+        <div class="button">
+          <input type="submit" value="Calculer mon empreinte" />
+           </div>
+
+           <h1 href="eradiquempreinte.html" target=_blank>Eradique ton empreinte de CO2</h1>
+       
+            <a id="button" href="eradiquempreinte.html" target=_blank> j'y cours j'y vole </a>
+        
+      </form>
+    </section>
+    <div class="logoright">
+    <img class="pvr" src="Images/Planetet.jpg" class="logo_header" alt="logo de commit tree">
+    <img class="pvr" src="Images/Planpb.jpg" class="logo_header" alt="logo de commit tree">
+  </div>
+  </div>
+
+
+    <!-- ajout du footer -->
+    <?php require("footer.html"); ?>
+
+</body>
+
+<!-- appel des fonctions javascript-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+<script type="text/javascript" src="javascript.js"></script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <title>Commit tree</title>
+    
+</head>
+<body>
+    <!-- ajout du header -->
+    <?php require("header.html"); ?>
+
+    <div id="menuPleinePage">
+        <!-- info menu dynamique-->
+
+        <div class="sousMenuPleineLargeur">acceuil</div>
+        <div class="sousMenuPleineLargeur">Calcul ton Empreinte</div>
+        <div class="sousMenuPleineLargeur">Eradique ton impact</div>
+        <div class="sousMenuPleineLargeur">Notre histoire</div>
+
+
+    </div>
+
+
+    <!-- debut baniere, section laurent-->
+    <section id="calculco">
+            <div class="button">
+                <h1>Calcul ton empreinte CO2</h1>    
+                <a href="calculempreinte.php" >
+                <input type="submit" value="Go!! Vers les calculs" />
+                </a> 
+            </div>
+            <div class="button">
+                <h1>Eradique ton empreinte CO2</h1>
+                <a href="eradiquempreinte.php" >
+                <input type="submit" value="Go!! Vers l'action" />
+                </a>
+            </div>
+     </section>
+<!-- fin section baniere, laurent-->
+
+
+<!-- debut section centrale par thierry-->
+
+    <section id="centrale_container">
+        
+            <div class="bloc_left">
+               
+                <div class="page_left"> 
+                
+                    <div class="image_section_centrale"><img src="Images/terrain_foot.png" alt ="Terrain de foot au milieu de la forêt"></div>
+                    
+                        <div class="article_left"> 
+                           
+                            <h1 class="left_title">La déforestation : un terrain de foot toutes les 2 secondes !!!!</h1>
+
+                            <p>Lisez l'article tickethic.fr : <br>
+                                 <a href="https://www.tickethic.fr/blog/deforestation-1-terrain-de-foot-toutes-les-2-secondes/">1 terrain de foot toutes les 2 secondes</a></p>
+                       
+                        </div>
+                </div>   
+                        
+            <img src="Images/logo_commitTree.png"  class="logo_left" alt="logo commit tree">
+            
+            </div>
+
+            <div class="bloc_center">
+
+                <div class="page_center">
+        
+                    <div class="image_section_centrale"><img src="Images/téléchargement.jpeg" alt="Petit arbuste fraîchement planté"></div>
+
+                        <div class="article_center">
+                        
+                            <h1 class="center_title">Nos actions réalisées</h1>
+                        
+                            <p>Tuto pour t'apprendre à planter ton arbre, Mister Green. <a href="https://www.youtube.com/watch?v=TbO_6O9OlX8"><br>Ca va pas être facile et c'est salissant, mais c'est gentil pour la planète.</a></p>
+                       
+                        </div>
+                        
+                </div>
+
+                <img src="Images/logo_commitTree.png" class="logo_center" alt="logo commit tree">
+            
+            </div> 
+
+            <div class="bloc_right">
+
+                <div class="page_right">
+
+                    <div class="image_section_centrale"><img src="Images/Shrek.png" alt="Shrek and Friends"></div>
+                        
+                        <div class="article_right">
+                            
+                            <h1 class="right_title">Témoignages</h1>
+                    
+                            <p>Shrek et ses amis ont replanté tout seul la forêt "En Chantier", pour lutter contre leurs propres émanations de CO2 et de méthane : <a href="https://youtu.be/yHlOeFjxMBE">ils en sont très fiers !</a></p>
+                         
+                        </div>
+
+                </div>       
+                       
+            <img src="Images/logo_commitTree.png " class="logo_right" alt="logo commit tree">
+                        
+            </div>
+        
+               
+        
+    </section>
+
+    <!-- ajout du footer -->
+    <?php require("footer.html"); ?>
+
+</body>
+
+<!-- appel des fonctions javascript-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+<script type="text/javascript" src="javascript.js"></script>
+
+</html>
